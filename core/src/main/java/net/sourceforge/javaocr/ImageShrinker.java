ce3c076e1ea3001aeb548371d08d18e735d4d27b
Eric Kitur <kitur.eric@yahoo.com>
2011-05-30 18:03:20 +0300

    Separated core from the Pom-Parent

--- core/src/main/java/net/sourceforge/javaocr/ImageShrinker.java
+++
@@ -1,9 +0,0 @@
-package net.sourceforge.javaocr;
-
-/**
- * shrink image in controlled way
- * TODO: do we need separate interface for it??????
- */
-public interface ImageShrinker {
-    Image shrink(Image source);
-}
